272f22e7efbecc167f27ed6c7441f59fa8cfba00
Ankita Sharma <ankitasharma1993@gmail.com>
2018-01-08 17:11:50 +0530

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} =require('./models/user.js');
@@ -28,9 +29,25 @@ app.get('/todos', (req, res) => {
     })
 })
 
-app.get('/todos/:id', () => {
-    req.params
+app.get('/todos/:id', (req, res) => {
+    var id =req.params.id;
+    if(!ObjectID.isValid(id)){
+       return res.status(404).send();
+    }
+    
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+         return res.status(404).send();   
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+    
+    
 })
+
+// GET /todos/
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
