0cda02d6845aa2a8d38db9de275b3ad256fd3dfc
Ankita Sharma <ankitasharma1993@gmail.com>
2018-01-12 20:02:03 +0530

    Added Patch /todos/id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
-var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} =require('./models/user.js');
@@ -66,7 +67,31 @@ app.delete('/todos/:id', (req, res) => {
     });
 });
 
-// GET /todos/
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();  //javascript timestamp is a number in milliseconds from jan 1 1970 
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    
+    Todo.findByIdAndUpdate(id, {$set: body}, { new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
